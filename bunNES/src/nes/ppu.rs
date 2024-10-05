d66b1c64e3f85e361e22e5ae3b6db2452ed903dc
Maya Longbunny <msymnick@googlemail.com>
2024-10-05 23:46:59 +0200

    PPU progress

--- bunNES/src/nes/ppu.rs
+++ bunNES/src/nes/ppu.rs
@@ -2,6 +2,7 @@ use std::process::exit;
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 use bit::BitIndex;
+use bitflags::bitflags;
 use rand::random;
 use crate::nes::cpu::{HEIGHT, RenderImage, WIDTH};
 use crate::nes::rom::Cartridge;
@@ -12,14 +13,14 @@ const MAX_DOT_COUNT: u32 = 283 * 242;
 #[allow(unused_variables, dead_code)]
 #[derive(Debug)]
 pub struct Ppu {
-    ppu_ctrl: u8,
-    ppu_mask: u8,
-    ppu_status: u8,
+    ppu_ctrl: PpuCtrl,
+    ppu_mask: PpuMask,
+    ppu_status: PpuStatus,
     oam_addr: u8,
     oam_data: u8,
     ppu_scroll: u8,
-    ppu_addr: u8,
-    ppu_data: u8,
+    ppu_addr: PpuAddr,
+    ppu_data: PpuData,
     oam_dma: u8,
 
     cartridge: Arc<Cartridge>,
@@ -29,6 +30,52 @@ pub struct Ppu {
     ppu_cycle_count: u64,
     dot_count: u64,
 }
+
+#[derive(Debug)]
+struct PpuCtrl(u8);
+
+bitflags! {
+    impl PpuCtrl: u8 {
+        const base_nametable   = 0b0000_0011;
+        const vram_addr_inc    = 0b0000_0100;
+        const spr_ptrn_addr    = 0b0000_1000;
+        const bgr_ptrn_addr    = 0b0001_0000;
+        const spr_size         = 0b0010_0000;
+        const ppu_master_slave = 0b0100_0000;
+        const gen_nmi_vblank   = 0b1000_0000;
+    }
+}
+
+#[derive(Debug)]
+struct PpuMask(u8);
+
+bitflags! {
+    impl PpuMask: u8 {
+        const grayscale         = 0b0000_0001;
+        const show_bgr_leftmost = 0b0000_0010;
+        const show_spr_leftmost = 0b0000_0100;
+        const show_bgr          = 0b0000_1000;
+        const show_spr          = 0b0001_0000;
+        const emphasize_red     = 0b0010_0000;
+        const emphasize_grn     = 0b0100_0000;
+        const emphasize_blu     = 0b1000_0000;
+    }
+}
+
+#[derive(Debug)]
+struct PpuStatus(u8);
+
+bitflags! {
+    impl PpuStatus: u8 {
+        const stale_bus    = 0b0001_1111;
+        const spr_oflo     = 0b0010_0000;
+        const spr_0_hit    = 0b0100_0000;
+        const vblank_start = 0b1000_0000;
+    }
+}
+
+type PpuAddr = u8;
+type PpuData = u8;
 
 struct PatternTable {
     tbl: [u8; 256 * 16 * 2],
@@ -55,9 +102,9 @@ impl Ppu {
     pub fn new(cartridge: Arc<Cartridge>) -> Ppu {
 
         Ppu {
-            ppu_ctrl: 0,
-            ppu_mask: 0,
-            ppu_status: 0,
+            ppu_ctrl: PpuCtrl(0),
+            ppu_mask: PpuMask(0),
+            ppu_status: PpuStatus(255),
             oam_addr: 0,
             oam_data: 0,
             ppu_scroll: 0,
@@ -76,14 +123,14 @@ impl Ppu {
 
     pub fn register(&mut self, register: u8) -> u8 {
         let value = match register {
-            0 => self.ppu_ctrl,
-            1 => self.ppu_mask,
+            0 => self.ppu_ctrl.0,
+            1 => self.ppu_mask.0,
             2 => {
-                let ppu_status = self.ppu_status;
+                
                 // set vblank to false after read
                 // https://www.nesdev.org/wiki/PPU_registers#PPUSTATUS
-                self.ppu_status.set_bit(7, false);
-                ppu_status
+                self.ppu_status.bits().set_bit(7, false);
+                self.ppu_status.0
             },
             3 => self.oam_addr,
             4 => self.oam_data,
@@ -101,9 +148,9 @@ impl Ppu {
         // delay until ppu boots i guess?
         let mut could_write = true;
         match register {
-            0 => self.ppu_ctrl = value,
-            1 => self.ppu_mask = value,
-            2 => self.ppu_status = value,
+            0 => self.ppu_ctrl.0 = value,
+            1 => self.ppu_mask.0 = value,
+            2 => self.ppu_status.0 = value,
             3 => self.oam_addr = value,
             4 => self.oam_data = value,
             5 => self.ppu_scroll = value,
@@ -145,14 +192,14 @@ impl Ppu {
             },
             241..=260 => {
                 if self.ppu_cycle_count % 340 == 1 {
-                    self.ppu_status.set_bit(7, true);
+                    self.ppu_status.bits().set_bit(7, true);
                 }
             },
             261 => {
                 // or -1
                 // dummy
                 if self.ppu_cycle_count % 340 == 1 {
-                    self.ppu_status.set_bit(7, false);
+                    self.ppu_status.bits().set_bit(7, false);
                 }
             }
             _ => panic!("scanline not matched: {scanline}")
